0275f1a046b81178c5070f97b2a0d237692c9084
Prats <pratyushgiri133@gmail.com>
2022-06-23 18:56:51 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get(url='https://opentdb.com/api.php?amount=10&type=boolean')
+response.raise_for_status()
+data = response.json()
+
+print(data)
+
+question_data = data["results"]
